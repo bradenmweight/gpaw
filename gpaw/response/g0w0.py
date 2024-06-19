8e1897a0189f6715c6b1fb5acab7076b2bf86e1d
Mikael Kuisma <kuisma@dtu.dk>
2024-06-19 18:20:53 +0200

    not

--- gpaw/response/g0w0.py
+++ gpaw/response/g0w0.py
@@ -1204,7 +1204,7 @@ class G0W0(G0W0Calculator):
                     'nbands cannot be supplied with ecut-extrapolation.')
 
         if ppa:
-            assert integrate_gamma.is_Wigner_Seitz, "TODO"
+            assert not integrate_gamma.is_Wigner_Seitz, "TODO"
             # use small imaginary frequency to avoid dividing by zero:
             frequencies = [1e-10j, 1j * E0]
 
