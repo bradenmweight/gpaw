c8bbef8426d7797498217a925d08151c2132a5c2
Mikael Kuisma <kuisma@dtu.dk>
2024-06-20 22:39:28 +0200

    simplify test

--- gpaw/test/response/test_mpa_poly_C.py
+++ gpaw/test/response/test_mpa_poly_C.py
@@ -9,7 +9,7 @@ def mpa_py(omega, f, omegat_nGG, W_nGG, eta, factor):
     x2_nGG = (1.0 - f) / (omega - omegat_nGG + 1j * eta)
 
     x_GG = 2 * factor * np.sum(W_nGG * (x1_nGG + x2_nGG),
-                               axis=0)  # Why 2 here
+                               axis=0)
 
     eps = 0.0001 / Ha
     xp_nGG = f / (omega + eps + omegat_nGG - 1j * eta)
@@ -17,7 +17,7 @@ def mpa_py(omega, f, omegat_nGG, W_nGG, eta, factor):
     xm_nGG = f / (omega - eps + omegat_nGG - 1j * eta)
     xm_nGG += (1.0 - f) / (omega - eps - omegat_nGG + 1j * eta)
     dx_GG = 2 * factor * np.sum(W_nGG * (xp_nGG - xm_nGG) / (2 * eps),
-                                axis=0)  # Why 2 here
+                                axis=0)
     return x_GG, dx_GG
 
 
@@ -27,14 +27,11 @@ def test_residues(in_tmp_dir, f):
     eta = 0.1 * Ha
     nG = 5
     npols = 10
-    omegat_nGG = np.empty((npols, nG, nG), dtype=np.complex128)
-    W_nGG = np.empty((npols, nG, nG), dtype=np.complex128)
     omega = 0.5
 
     rng = np.random.default_rng(seed=1)
     omegat_nGG = rng.random((npols, nG, nG)) * 0.05 + 5.5 - 0.01j
-    W_nGG[:] = rng.random((npols, nG, nG))
-    W_nGG = np.ascontiguousarray(W_nGG)
+    W_nGG = np.array(rng.random((npols, nG, nG)), dtype=complex)
 
     x_GG_py, dx_GG_py = mpa_py(omega, f, omegat_nGG, W_nGG, eta, factor)
 
