0ddf6c7c0d0a54cbc9366b0776ef0c6ecf5504f5
Thorbjørn Skovhus <s134264@student.dtu.dk>
2024-06-24 15:05:47 +0200

    add a modern day eels test of a 2d system

--- gpaw/test/response/test_graphene_EELS.py
+++ gpaw/test/response/test_graphene_EELS.py
@@ -7,12 +7,46 @@ from ase.parallel import parprint as pp
 from gpaw import GPAW
 from gpaw.response.df import DielectricFunction
 from gpaw.mpi import world
+from gpaw.test import findpeak
+
+
+@pytest.mark.dielectricfunction
+@pytest.mark.response
+def test_graphene_EELS(in_tmp_dir, gpw_files):
+    # Test values
+    q_qc = [
+        [0, 0, 0],
+        [1 / 3, 0, 0],
+    ]
+    refs_q = [
+        # peak frequency and intensity (rows), w.o./w. local-field corr. (cols)
+        np.array([[7.57, 7.66], [3.78, 4.01]]),
+        np.array([[5.86, 6.29], [0.99, 0.48]]),
+    ]
+    # Calculation
+    dfcalc = DielectricFunction(gpw_files['graphene_pw'], truncation='2D',
+                                frequencies=np.linspace(0., 9., 141),
+                                eta=0.3, ecut=150, rate='eta', hilbert=False)
+    for q_c, refs in zip(q_qc, refs_q):
+        epsinv = dfcalc.get_inverse_dielectric_function(q_c=q_c)
+        omega_w, eels0_w, eels_w = epsinv.eels_spectrum().arrays
+
+        # import matplotlib.pyplot as plt
+        # plt.plot(omega_w, eels0_w)
+        # plt.plot(omega_w, eels_w)
+        # plt.show()
+
+        # Test against reference
+        e0, I0 = findpeak(omega_w, eels0_w)
+        e, I = findpeak(omega_w, eels_w)
+        assert np.array([e0, e]) == pytest.approx(refs[0, :], abs=0.05)
+        assert np.array([I0, I]) == pytest.approx(refs[1, :], abs=0.02)
 
 
 @pytest.mark.dielectricfunction
 @pytest.mark.response
 @pytest.mark.skip(reason='TODO')
-def test_graphene_EELS():
+def test_graphene_EELS_old():
     system = Graphene(symbol='C',
                       latticeconstant={'a': 2.45, 'c': 1.0},
                       size=(1, 1, 1))
