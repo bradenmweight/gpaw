Fix eigenstates residual error calculation in etdm lcao eigensolver

gpaw/directmin/functional/lcao/pz.py
@@ -178,7 +178,7 @@ class PZSICLCAO:
             norm.append(np.dot(hc_mn[:, i].conj(),
                                hc_mn[:, i]).real * kpt.f_n[i])
 
-        error = sum(norm) * Hartree ** 2 / wfs.nvalence
+        error = sum(norm)
         del rhs2, hc_mn, norm
         timer.stop('Residual')
 