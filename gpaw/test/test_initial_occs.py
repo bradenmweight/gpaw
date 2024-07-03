65b8e90906249635527b7ba636f5dc375c03a8ee
Jens Jørgen Mortensen <jj@smoerhul.dk>
2024-07-03 14:12:33 +0200

    Test all potentials

--- gpaw/test/test_initial_occs.py
+++ gpaw/test/test_initial_occs.py
@@ -23,11 +23,14 @@ def test_initial_occs(symbol, M, n):
 def test_ca():
     """Our Ca PAW-potential only has completely filled partial waves
     (3s, 3p and 4s).  So, a magmom of 1 can't be accomodated.  GPAW
-    should still work though!  We should make a new Ca-potential.
+    should still work though!  Same problem for Ne.
     """
-    s = create_setup('Ca')
-    f_si = s.calculate_initial_occupation_numbers(magmom=1,
-                                                  hund=False,
-                                                  charge=0,
-                                                  nspins=2)
-    print(f_si)
+    from ase.data import chemical_symbols
+    for sy in chemical_symbols:
+        try:
+            s = create_setup(sy)
+        except FileNotFoundError:
+            continue
+        f_si = s.calculate_initial_occupation_numbers(
+            magmom=1, hund=False, charge=0, nspins=2)
+        print(sy, f_si)
