948637498b12183ecd1e04c26a95afc671d24a22
Jens Jørgen Mortensen <jj@smoerhul.dk>
2024-07-03 14:03:13 +0200

    Allow magmom=1 for Ca-atom

--- gpaw/test/test_initial_occs.py
+++ gpaw/test/test_initial_occs.py
@@ -18,3 +18,16 @@ def test_initial_occs(symbol, M, n):
     assert abs(magmom - M) < 1e-10
     N = ((f_si[0] - f_si[1]) != 0).sum()
     assert n == N, 'Wrong # of values have changed'
+
+
+def test_ca():
+    """Our Ca PAW-potential only has completely filled partial waves
+    (3s, 3p and 4s).  So, a magmom of 1 can't be accomodated.  GPAW
+    should still work though!  We should make a new Ca-potential.
+    """
+    s = create_setup('Ca')
+    f_si = s.calculate_initial_occupation_numbers(magmom=1,
+                                                  hund=False,
+                                                  charge=0,
+                                                  nspins=2)
+    print(f_si)
